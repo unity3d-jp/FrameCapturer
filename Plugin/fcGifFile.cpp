46d884c8d16417d14e41ffb792a62e9b12f581e8
i-saint <saint.skr@gmail.com>
2015-05-29 01:48:19 +0900

    optimization: added async write

--- Plugin/fcGifFile.cpp
+++ Plugin/fcGifFile.cpp
@@ -14,7 +14,8 @@ public:
 public:
     fcGifContext(const char *path, fcGifConfig *conf);
     ~fcGifContext();
-    void addFrameTask(int i);
+    void scrape();
+    void addFrameTask(int frame, bool local_palette, std::string &buf);
     void addFrame(void *tex);
 
 private:
@@ -22,10 +23,11 @@ private:
     fcGifConfig m_conf;
     std::string m_path;
     std::vector<WorkData> m_work;
+    std::list<std::string> m_frames;
     tbb::task_group m_tasks;
     jo_gif_t m_gif;
     void *m_tmp_tex;
-    volatile int m_frame_count;
+    int m_frame;
     volatile int m_active_task_count;
 };
 
@@ -35,7 +37,7 @@ fcGifContext::fcGifContext(const char *path, fcGifConfig *conf)
     , m_conf(*conf)
     , m_path(path)
     , m_tmp_tex(nullptr)
-    , m_frame_count(0)
+    , m_frame(0)
     , m_active_task_count(0)
 {
     m_gif = jo_gif_start(path, m_conf.width, m_conf.height, 0, 255);
@@ -49,16 +51,51 @@ fcGifContext::fcGifContext(const char *path, fcGifConfig *conf)
 fcGifContext::~fcGifContext()
 {
     m_tasks.wait();
+    scrape();
+    for (auto &buf : m_frames) {
+        fwrite(buf.c_str(), buf.size(), 1, m_gif.fp);
+    }
     jo_gif_end(&m_gif);
     fcGetGraphicsDevice()->releaseTmpTexture(m_tmp_tex);
 }
 
-void fcGifContext::addFrameTask(int f)
+void fcGifContext::addFrameTask(int frame, bool local_palette, std::string &buf)
 {
-    int w = f % m_conf.max_active_tasks;
-    bool update_palette = m_conf.keyframe != 0 && f%m_conf.keyframe == 0;
-    jo_gif_frame(&m_gif, (unsigned char*)&m_work[w].raw[0], 3, update_palette);
+    int w = frame % m_conf.max_active_tasks;
+
+    std::stringstream os(buf, std::ios::binary | std::ios::out);
+    jo_gif_frame(os, frame, &m_gif, (unsigned char*)&m_work[w].raw[0], 3, local_palette);
+    buf = os.str();
     fcAtomicDecrement(&m_active_task_count);
+}
+
+void fcGifContext::scrape()
+{
+    int min_frames = std::max<int>(m_conf.max_active_tasks, 1);
+
+    // 最大フレーム数超えてたら間引く
+    if (m_conf.max_frame > 0)
+    {
+        while (m_conf.max_frame > min_frames && m_frames.size() > m_conf.max_frame)
+        {
+            // 最初のフレームに含まれる情報削除すると読めなくなるので、2 番目以降から消す
+            m_frames.erase(++m_frames.begin());
+        }
+    }
+
+    // 最大容量超えてたら間引く
+    if (m_conf.max_data_size > 0)
+    {
+        size_t size = 14; // gif header + footer size
+        for (auto &i : m_frames) { size += i.size(); }
+        while (m_frames.size() > min_frames && size > m_conf.max_data_size)
+        {
+            // 同上
+            auto iter = ++m_frames.begin();
+            size -= iter->size();
+            m_frames.erase(iter);
+        }
+    }
 }
 
 void fcGifContext::addFrame(void *tex)
@@ -68,20 +105,36 @@ void fcGifContext::addFrame(void *tex)
         m_tmp_tex = fcGetGraphicsDevice()->createTmpTexture(m_conf.width, m_conf.height, fcE_ARGB32);
     }
 
-
-    //if (m_active_task_count >= m_conf.max_active_tasks)
+    if (m_active_task_count >= m_conf.max_active_tasks)
     {
-        m_tasks.wait();
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        if (m_active_task_count >= m_conf.max_active_tasks)
+        {
+            m_tasks.wait();
+        }
     }
     fcAtomicIncrement(&m_active_task_count);
-    int f = ++m_frame_count;
-    int w = f % m_conf.max_active_tasks;
+    int frame = m_frame++;
+    int w = frame % m_conf.max_active_tasks;
 
     // get raw framebuffer
     fcGetGraphicsDevice()->copyTextureData(&m_work[w].raw[0], tex, m_tmp_tex, m_conf.width, m_conf.height, fcE_ARGB32);
 
-    // compress asynchronously
-    m_tasks.run([=](){ addFrameTask(f % m_conf.max_active_tasks); });
+    scrape();
+    m_frames.push_back(std::string());
+    std::string& buf = m_frames.back();
+
+    bool local_palette = frame==0 || (m_conf.keyframe != 0 && frame % m_conf.keyframe == 0);
+    if (local_palette) {
+        // パレットの新は前後のフレームに影響をあたえるため、同期更新でなければならない
+        m_tasks.wait();
+        addFrameTask(frame, local_palette, buf);
+    }
+    else
+    {
+        // compress asynchronously
+        m_tasks.run([this, frame, local_palette, &buf](){ addFrameTask(frame, local_palette, buf); });
+    }
 }
 
 
