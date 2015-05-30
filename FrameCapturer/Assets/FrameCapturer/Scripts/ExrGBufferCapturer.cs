539f08d1ef04fbf72a2814248eda41ffa3bda793
i-saint <saint.skr@gmail.com>
2015-05-30 19:58:56 +0900

    ExrGBufferCapturer wip

--- FrameCapturer/Assets/FrameCapturer/Scripts/ExrGBufferCapturer.cs
+++ FrameCapturer/Assets/FrameCapturer/Scripts/ExrGBufferCapturer.cs
@@ -4,6 +4,10 @@ using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Rendering;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif // UNITY_EDITOR
+
 
 [AddComponentMenu("FrameCapturer/ExrGBufferCapturer")]
 [RequireComponent(typeof(Camera))]
@@ -13,14 +17,12 @@ public class ExrGBufferCapturer : MonoBehaviour
     public bool m_capture_on_postrender = true;
     public int m_max_active_tasks = 10;
 
-    public Shader m_copy_gbuffer;
-    public Shader m_copy_depth;
+    public Shader m_sh_copy;
 
     IntPtr m_exr;
     int m_frame;
     Mesh m_quad;
-    Material m_mat_gbuffer;
-    Material m_mat_depth;
+    Material m_mat_copy;
 
     /// m_gbuffer[0] : rgb: diffuse   a: occulusion
     /// m_gbuffer[1] : rgb: specular  a: smoothness
@@ -48,6 +50,14 @@ public class ExrGBufferCapturer : MonoBehaviour
         }
     }
 
+
+#if UNITY_EDITOR
+    void Reset()
+    {
+        m_sh_copy = AssetDatabase.LoadAssetAtPath("Assets/FrameCapturer/Shaders/CopyFrameBuffer.shader", typeof(Shader)) as Shader;
+    }
+#endif // UNITY_EDITOR
+
     void OnEnable()
     {
         m_quad = FrameCapturerUtils.CreateFullscreenQuad();
@@ -58,11 +68,10 @@ public class ExrGBufferCapturer : MonoBehaviour
             cam.depthTextureMode = DepthTextureMode.Depth;
         }
 
-        m_mat_gbuffer = new Material(m_copy_gbuffer);
-        m_mat_depth = new Material(m_copy_depth);
+        m_mat_copy = new Material(m_sh_copy);
 
         m_gbuffer = new RenderTexture[4];
-        m_rt_gbuffer = new RenderBuffer[4];
+        m_rt_gbuffer = new RenderBuffer[5];
         for (int i = 0; i < m_gbuffer.Length; ++i )
         {
             m_gbuffer[i] = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 0, RenderTextureFormat.ARGBHalf);
@@ -74,10 +83,10 @@ public class ExrGBufferCapturer : MonoBehaviour
             m_depth = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 0, RenderTextureFormat.RFloat);
             m_depth.filterMode = FilterMode.Point;
             m_depth.Create();
+            m_rt_gbuffer[4] = m_depth.colorBuffer;
         }
 
         FrameCapturer.AddLibraryPath();
-
         FrameCapturer.fcExrConfig conf;
         conf.max_active_tasks = m_max_active_tasks;
         m_exr = FrameCapturer.fcExrCreateContext(ref conf);
@@ -95,23 +104,19 @@ public class ExrGBufferCapturer : MonoBehaviour
     {
         if (!m_capture_on_postrender) { return; }
 
-        m_mat_gbuffer.SetPass(0);
+        int frame = m_frame++;
+
+        m_mat_copy.SetPass(2);
         Graphics.SetRenderTarget(m_rt_gbuffer, m_gbuffer[0].depthBuffer);
         Graphics.DrawMeshNow(m_quad, Matrix4x4.identity);
 
-        m_mat_depth.SetPass(0);
-        Graphics.SetRenderTarget(m_depth);
-        Graphics.DrawMeshNow(m_quad, Matrix4x4.identity);
-        Graphics.SetRenderTarget(null);
-
-        WriteFile( m_gbuffer[0], m_output_folder + "/diffuse_"    + m_frame.ToString("0000") + ".exr", 0x7);
-        WriteFile2(m_gbuffer[0], m_output_folder + "/occulusion_" + m_frame.ToString("0000") + ".exr", 0x8);
-        WriteFile( m_gbuffer[1], m_output_folder + "/specular_"   + m_frame.ToString("0000") + ".exr", 0x7);
-        WriteFile2(m_gbuffer[1], m_output_folder + "/smoothness_" + m_frame.ToString("0000") + ".exr", 0x8);
-        WriteFile( m_gbuffer[2], m_output_folder + "/normal_"     + m_frame.ToString("0000") + ".exr", 0x7);
-        WriteFile( m_gbuffer[3], m_output_folder + "/emission_"   + m_frame.ToString("0000") + ".exr", 0x7);
-        WriteFile(      m_depth, m_output_folder + "/depth_"      + m_frame.ToString("0000") + ".exr");
-
-        ++m_frame;
+        string sframe = frame.ToString("0000");
+        WriteFile( m_gbuffer[0], m_output_folder + "/diffuse_"    + sframe + ".exr", 0x7);
+        WriteFile2(m_gbuffer[0], m_output_folder + "/occulusion_" + sframe + ".exr", 0x8);
+        WriteFile( m_gbuffer[1], m_output_folder + "/specular_"   + sframe + ".exr", 0x7);
+        WriteFile2(m_gbuffer[1], m_output_folder + "/smoothness_" + sframe + ".exr", 0x8);
+        WriteFile( m_gbuffer[2], m_output_folder + "/normal_"     + sframe + ".exr", 0x7);
+        WriteFile( m_gbuffer[3], m_output_folder + "/emission_"   + sframe + ".exr", 0x7);
+        WriteFile(      m_depth, m_output_folder + "/depth_"      + sframe + ".exr");
     }
 }
