cdfdb2d4107845e37846e0d2567cef339a6c135f
i-saint <saint.skr@gmail.com>
2015-06-12 10:46:11 +0900

    minor improvements

--- Plugin/mp4test/fcH264Encoder.h
+++ Plugin/mp4test/fcH264Encoder.h
@@ -1,7 +1,6 @@
 ﻿#ifndef fcMP4Encoder_h
 #define fcMP4Encoder_h
 
-#include <functional>
 #include <string>
 #include "fcColorSpace.h"
 
@@ -12,12 +11,12 @@ class fcH264Encoder
 public:
     enum FrameType
     {
-        videoFrameTypeInvalid,    ///< encoder not ready or parameters are invalidate
-        videoFrameTypeIDR,        ///< IDR frame in H.264
-        videoFrameTypeI,          ///< I frame type
-        videoFrameTypeP,          ///< P frame type
-        videoFrameTypeSkip,       ///< skip the frame based encoder kernel
-        videoFrameTypeIPMixed     ///< a frame where I and P slices are mixing, not supported yet
+        FrameType_Invalid,    ///< encoder not ready or parameters are invalidate
+        FrameType_IDR,        ///< IDR frame in H.264
+        FrameType_I,          ///< I frame type
+        FrameType_P,          ///< P frame type
+        FrameType_Skip,       ///< skip the frame based encoder kernel
+        FrameType_IPMixed     ///< a frame where I and P slices are mixing, not supported yet
     };
     struct Result
     {
@@ -25,7 +24,7 @@ public:
         int size;
         FrameType type;
 
-        Result(void *d = nullptr, int s = 0, FrameType t = videoFrameTypeInvalid)
+        Result(void *d = nullptr, int s = 0, FrameType t = FrameType_Invalid)
             : data(d), size(s), type(t) {}
     };
 
