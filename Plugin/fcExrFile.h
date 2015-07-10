041f234c06dfca3fdabedc90837aa45c5102efd6
Gaetan Guidet <gatgui@gmail.com>
2015-07-11 00:16:06 +0900

    add option to treat "tex" pointer as raw pixels in fcExrAddLayer

--- Plugin/fcExrFile.h
+++ Plugin/fcExrFile.h
@@ -5,7 +5,7 @@ class fcIExrContext
 public:
     virtual void release() = 0;
     virtual bool beginFrame(const char *path, int width, int height) = 0;
-    virtual bool addLayer(void *tex, fcETextureFormat fmt, int channel, const char *name, bool flipY) = 0;
+    virtual bool addLayer(void *tex, fcETextureFormat fmt, int channel, const char *name, bool flipY, bool asPixels) = 0;
     virtual bool endFrame() = 0;
 protected:
     virtual ~fcIExrContext() {}
