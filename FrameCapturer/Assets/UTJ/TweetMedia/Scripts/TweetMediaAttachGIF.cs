f58801b1d97037cbd5fe47b99e3e2e5941c0ce66
i-saint <saint.skr@gmail.com>
2016-02-17 19:23:56 +0900

    TweetMediaAttachMP4

---
+++ FrameCapturer/Assets/UTJ/TweetMedia/Scripts/TweetMediaAttachGIF.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace UTJ
+{
+    [RequireComponent(typeof(TweetMedia))]
+    public class TweetMediaAttachGIF : MonoBehaviour
+    {
+        public GifCapturerHUD m_capturer_hud;
+        public UnityEngine.UI.Toggle m_toggle_screenshot;
+        TweetMedia m_tweet_media;
+
+
+        void AttachScreenshot(TweetMedia.TweetStateCode code)
+        {
+            if (code == TweetMedia.TweetStateCode.Begin)
+            {
+                if (!m_toggle_screenshot.isOn) { return; }
+                m_toggle_screenshot.isOn = false;
+
+                IGifCapturer capturer = m_capturer_hud.m_capturer;
+                {
+                    int begin = m_capturer_hud.begin_frame;
+                    int end = m_capturer_hud.end_frame;
+                    int expected_data_size = capturer.GetExpectedFileSize(begin, end);
+                    IntPtr data = Marshal.AllocHGlobal(expected_data_size);
+                    int actual_data_size = capturer.WriteMemory(data, begin, end);
+                    m_tweet_media.AddMedia(data, actual_data_size, TweetMediaPlugin.tmEMediaType.GIF);
+                    Marshal.FreeHGlobal(data);
+                }
+            }
+        }
+
+        void Start()
+        {
+            if (m_capturer_hud == null)
+            {
+                Debug.LogError("TweetMediaAttachGIF: m_capturer_hud is null");
+            }
+            m_tweet_media = GetComponent<TweetMedia>();
+            m_tweet_media.AddTweetEventHandler(AttachScreenshot);
+        }
+    }
+}
---
+++ FrameCapturer/Assets/UTJ/TweetMedia/Scripts/TweetMediaAttachMP4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace UTJ
+{
+    [RequireComponent(typeof(TweetMedia))]
+    public class TweetMediaAttachMP4 : MonoBehaviour
+    {
+        public MP4CapturerHUD m_capturer_hud;
+        public UnityEngine.UI.Toggle m_toggle_screenshot;
+        TweetMedia m_tweet_media;
+
+
+        void AttachScreenshot(TweetMedia.TweetStateCode code)
+        {
+            if (code == TweetMedia.TweetStateCode.Begin)
+            {
+                if (!m_toggle_screenshot.isOn) { return; }
+                m_toggle_screenshot.isOn = false;
+
+                IMP4Capturer capturer = m_capturer_hud.m_capturer;
+                capturer.FlushMP4();
+                m_tweet_media.AddMediaFile(capturer.GetOutputPath());
+            }
+        }
+
+        void Start()
+        {
+            if (m_capturer_hud == null)
+            {
+                Debug.LogError("TweetMediaAttachMP4: m_capturer_hud is null");
+            }
+            m_tweet_media = GetComponent<TweetMedia>();
+            m_tweet_media.AddTweetEventHandler(AttachScreenshot);
+        }
+    }
+}
