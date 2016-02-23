ab710f22a920f3eaf170b9541a491c89e7a85b8d
i-saint <saint.skr@gmail.com>
2016-02-23 22:59:53 +0900

    forgot to add / delete some files

---
+++ Plugin/Tests/Test.cpp
@@ -0,0 +1,14 @@
+#include "TestCommon.h"
+
+void PngTest();
+void ExrTest();
+void GifTest();
+void MP4Test();
+
+int main(int argc, char *argv[])
+{
+    PngTest();
+    ExrTest();
+    GifTest();
+    MP4Test();
+}
