ca8ee5bcb9af8629ca68b6bd7cfefc64bcec16d4
i-saint <saint.skr@gmail.com>
2016-03-01 18:10:37 +0900

    convert test passed!

--- Plugin/Tests/ConvertTest.cpp
+++ Plugin/Tests/ConvertTest.cpp
@@ -1,18 +1,21 @@
 #include "TestCommon.h"
 
-void fcConvert(void *dst, size_t dstsize, fcPixelFormat dstfmt, const void *src, size_t srcsize, fcPixelFormat srcfmt);
+const void* fcConvert(void *dst, fcPixelFormat dstfmt, const void *src, fcPixelFormat srcfmt, size_t size);
 
 const int Width = 320;
 const int Height = 240;
 
 
-template<class Dst, class Src>
-void ConvertTestImpl(fcIPngContext *ctx, std::vector<Src>& src, const char *filename)
+template<class Src, class Dst>
+void ConvertTestImpl(fcIPngContext *ctx, std::vector<Src>& src)
 {
+    char filename[128];
+    sprintf(filename, "%s_to_%s.png", GetPixelFormat<Src>::getName(), GetPixelFormat<Dst>::getName());
+
     std::vector<Dst> dst;
     dst.resize(Width * Height);
-    fcConvert(&dst[0], GetPixelFormat<Dst>::value, &src[0], GetPixelFormat<Src>::value, src.size());
-    fcPngExportPixels(ctx, filename, &dst[0], Width, Height, GetPixelFormat<Dst>::value);
+    auto data = fcConvert(&dst[0], GetPixelFormat<Dst>::value, &src[0], GetPixelFormat<Src>::value, src.size());
+    fcPngExportPixels(ctx, filename, data, Width, Height, GetPixelFormat<Dst>::value);
 }
 
 void ConvertTest()
@@ -22,8 +25,36 @@ void ConvertTest()
     fcPngConfig conf;
     fcIPngContext *ctx = fcPngCreateContext(&conf);
 
-    std::vector<RGBAu8> video_frame(Width * Height);
-    CreateVideoData(&video_frame[0], Width, Height, 0);
+#define TestCases(SrcT)\
+{\
+    std::vector<SrcT> video_frame(Width * Height);\
+    CreateVideoData(&video_frame[0], Width, Height, 0);\
+    ConvertTestImpl<SrcT, RGBAu8>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, RGBu8>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, RGu8>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, Ru8>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, RGBAf16>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, RGBf16>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, RGf16>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, Rf16>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, RGBAf32>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, RGBf32>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, RGf32>(ctx, video_frame);\
+    ConvertTestImpl<SrcT, Rf32>(ctx, video_frame);\
+}
+
+    TestCases(RGBAu8);
+    TestCases(RGBu8);
+    TestCases(RGu8);
+    TestCases(Ru8);
+    TestCases(RGBAf16);
+    TestCases(RGBf16);
+    TestCases(RGf16);
+    TestCases(Rf16);
+    TestCases(RGBAf32);
+    TestCases(RGBf32);
+    TestCases(RGf32);
+    TestCases(Rf32);
 
     fcPngDestroyContext(ctx);
 
