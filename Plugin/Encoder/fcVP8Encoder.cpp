0e28d08e1aede26129d686a913d7e1b107e9104a
i-saint <saint.skr@gmail.com>
2016-10-19 17:21:29 +0900

    WebM wip

--- Plugin/Encoder/fcVP8Encoder.cpp
+++ Plugin/Encoder/fcVP8Encoder.cpp
@@ -3,8 +3,14 @@
 #include "fcI420.h"
 #include "fcVPXEncoder.h"
 
-#include "vpx/vpx/vpx_encoder.h"
-#include "vpx/vpx/vp8cx.h"
+#include "libvpx/vpx/vpx_encoder.h"
+#include "libvpx/vpx/vp8cx.h"
+
+struct VP8VideoFrame
+{
+    fcTime timestamp;
+    Buffer data;
+};
 
 class fcVP8Encoder : public fcIVPXEncoder
 {
@@ -15,7 +21,11 @@ public:
     bool encode(fcVPXFrame& dst, const fcI420Data& image, fcTime timestamp, bool force_keyframe) override;
 
 private:
-    fcVP8EncoderConfig m_conf;
+    fcVP8EncoderConfig  m_conf = {};
+    vpx_codec_ctx_t     m_vpx_ctx = {};
+    vpx_codec_iface_t   *m_vpx_iface = nullptr;
+    vpx_image_t         m_vpx_img = {};
+    fcTime              m_prev_timestamp = 0.0;
 };
 
 
@@ -27,10 +37,24 @@ fcIVPXEncoder* fcCreateVP8Encoder(const fcVP8EncoderConfig& conf)
 fcVP8Encoder::fcVP8Encoder(const fcVP8EncoderConfig& conf)
     : m_conf(conf)
 {
+    m_vpx_iface = vpx_codec_vp8_cx();
+
+    vpx_codec_enc_cfg_t vpx_config;
+    vpx_codec_enc_config_default(m_vpx_iface, &vpx_config, 0);
+    vpx_config.g_w = m_conf.width;
+    vpx_config.g_h = m_conf.height;
+    vpx_config.g_timebase.num = 1;
+    vpx_config.g_timebase.den = 1000000;
+    vpx_config.rc_target_bitrate = m_conf.target_bitrate;
+    vpx_codec_enc_init(&m_vpx_ctx, m_vpx_iface, &vpx_config, 0);
+
+    m_vpx_img.fmt = VPX_IMG_FMT_I420;
+    vpx_img_set_rect(&m_vpx_img, 0, 0, conf.width, conf.height);
 }
 
 fcVP8Encoder::~fcVP8Encoder()
 {
+    vpx_codec_destroy(&m_vpx_ctx);
 }
 
 void fcVP8Encoder::release()
@@ -40,5 +64,30 @@ void fcVP8Encoder::release()
 
 bool fcVP8Encoder::encode(fcVPXFrame& dst, const fcI420Data& image, fcTime timestamp, bool force_keyframe)
 {
-    return false;
+    vpx_image_t vpx_img;
+    vpx_codec_pts_t vpx_time = uint64_t(timestamp * 1000000.0);
+    vpx_enc_frame_flags_t vpx_flags = 0;
+    uint32_t duration = uint32_t((timestamp - m_prev_timestamp) * 1000000.0);
+    m_prev_timestamp = timestamp;
+    if (force_keyframe) {
+        vpx_flags |= VPX_EFLAG_FORCE_KF;
+    }
+
+    auto res = vpx_codec_encode(&m_vpx_ctx, &vpx_img, vpx_time, duration, vpx_flags, 0);
+    if (res != VPX_CODEC_OK) {
+        return false;
+    }
+
+    vpx_codec_iter_t iter = nullptr;
+    const vpx_codec_cx_pkt_t *pkt = nullptr;
+    while ((pkt = vpx_codec_get_cx_data(&m_vpx_ctx, &iter)) != nullptr) {
+        if (pkt->kind == VPX_CODEC_CX_FRAME_PKT) {
+            int keyframe = (pkt->data.frame.flags & VPX_FRAME_IS_KEY) != 0;
+            dst.timestamp = timestamp;
+            dst.data.append((char*)pkt->data.frame.buf, pkt->data.frame.sz);
+            dst.segments.push_back((int)pkt->data.frame.sz);
+        }
+    }
+
+    return true;
 }
--- Plugin/Encoder/fcWebMFile.cpp
+++ Plugin/Encoder/fcWebMFile.cpp
@@ -10,3 +10,110 @@
 #include "webm/mkvwriter.hpp"
 #include "webm/mkvmuxer.hpp"
 #include "webm/mkvmuxerutil.hpp"
+
+#ifdef _MSC_VER
+    #pragma comment(lib, "vpxmt.lib")
+#endif // _MSC_VER
+
+
+
+class fcWebMContext : public fcIWebMContext
+{
+public:
+    fcWebMContext(fcWebMConfig &conf, fcIGraphicsDevice *gd);
+    ~fcWebMContext() override;
+    void release() override;
+
+    void addOutputStream(fcStream *s) override;
+
+    bool addVideoFrameTexture(void *tex, fcPixelFormat fmt, fcTime timestamp) override;
+
+    bool addVideoFramePixels(const void *pixels, fcPixelFormat fmt, fcTime timestamp) override;
+
+    bool addAudioFrame(const float *samples, int num_samples, fcTime timestamp) override;
+
+private:
+    fcWebMConfig m_conf;
+    fcIGraphicsDevice *m_gdev = nullptr;
+    std::unique_ptr<fcIVPXEncoder> m_video_encoder;
+    std::unique_ptr<fcIVorbisEncoder> m_audio_encoder;
+};
+
+
+fcWebMContext::fcWebMContext(fcWebMConfig &conf, fcIGraphicsDevice *gd)
+    : m_conf(conf)
+    , m_gdev(gd)
+{
+    if (conf.video) {
+        fcVPXEncoderConfig econf;
+        econf.width = conf.video_width;
+        econf.height = conf.video_height;
+        econf.target_bitrate = conf.video_bitrate;
+
+        switch (conf.video_encoder) {
+        case fcWebMVideoEncoder::VP8:
+            m_video_encoder.reset(fcCreateVP8Encoder(econf));
+            break;
+        case fcWebMVideoEncoder::VP9:
+            m_video_encoder.reset(fcCreateVP8Encoder(econf));
+            break;
+        }
+    }
+
+    if (conf.audio) {
+        fcVorbisEncoderConfig econf;
+        econf.sampling_rate = conf.audio_sample_rate;
+        econf.num_channels = conf.audio_num_channels;
+        econf.target_bitrate = conf.audio_bitrate;
+
+        switch (conf.audio_encoder) {
+        case fcWebMAudioEncoder::Vorbis:
+            m_audio_encoder.reset(fcCreateVorbisEncoder(econf));
+            break;
+        case fcWebMAudioEncoder::Opus:
+            m_audio_encoder.reset(fcCreateOpusEncoder(econf));
+            break;
+        }
+    }
+}
+
+fcWebMContext::~fcWebMContext()
+{
+}
+
+void fcWebMContext::release()
+{
+    delete this;
+}
+
+void fcWebMContext::addOutputStream(fcStream *s)
+{
+    // todo
+}
+
+bool fcWebMContext::addVideoFrameTexture(void *tex, fcPixelFormat fmt, fcTime timestamp)
+{
+    if (!tex || !m_video_encoder || !m_gdev) { return false; }
+
+    //m_video_encoder->encode(timestamp);
+    return true;
+}
+
+bool fcWebMContext::addVideoFramePixels(const void *pixels, fcPixelFormat fmt, fcTime timestamp)
+{
+    if (!pixels || !m_video_encoder) { return false; }
+
+    return true;
+}
+
+bool fcWebMContext::addAudioFrame(const float *samples, int num_samples, fcTime timestamp)
+{
+    if (!samples || !m_audio_encoder) { return false; }
+
+    return true;
+}
+
+fcWebMAPI fcIWebMContext* fcWebMCreateContextImpl(fcWebMConfig &conf, fcIGraphicsDevice *gd)
+{
+    return new fcWebMContext(conf, gd);
+}
