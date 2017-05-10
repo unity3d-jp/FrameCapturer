fd25839e83e5b621f8560c683a36cdfbb4e236e8
i-saint <saint.skr@gmail.com>
2017-05-11 02:16:50 +0900

    FrameRecorder has moved to other repository

--- FrameCapturer/Assets/Unity/FrameRecorder/Scripts/Engine/FrameRecorderAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace UnityEngine.Recorder.FrameRecorder
-{
-    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
-    public class FrameRecorderClassAttribute : Attribute
-    {
-    }
-}
