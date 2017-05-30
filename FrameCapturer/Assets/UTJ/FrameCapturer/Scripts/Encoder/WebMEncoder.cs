d1e8b00081735c3ded5a6e8fa3047a4b6fef4052
i-saint <saint.skr@gmail.com>
2017-05-31 03:27:19 +0900

    minor improvements

--- FrameCapturer/Assets/UTJ/FrameCapturer/Scripts/Encoder/WebMEncoder.cs
+++ FrameCapturer/Assets/UTJ/FrameCapturer/Scripts/Encoder/WebMEncoder.cs
@@ -27,7 +27,7 @@ namespace UTJ.FrameCapturer
                 var sampleRate = AudioSettings.outputSampleRate;
                 if (sampleRate != 8000 && sampleRate != 12000 && sampleRate != 16000 && sampleRate != 24000 && sampleRate != 48000)
                 {
-                    Debug.LogError("Output sample rate must be 8000, 12000, 16000, 24000 or 48000 to use Opus audio encoder. Fallback to Vorbis.");
+                    Debug.LogError("Current output sample rate is " + sampleRate + ". It must be 8000, 12000, 16000, 24000 or 48000 to use Opus audio encoder. Fallback to Vorbis.");
                     m_config.audioEncoder = fcAPI.fcWebMAudioEncoder.Vorbis;
                 }
             }
