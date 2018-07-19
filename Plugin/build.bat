88ef79dec69b2b541a685409655e95c5c3b9b3ad
i-saint <saint.skr@gmail.com>
2018-07-19 19:03:36 +0900

    update build scripts

--- Plugin/build.bat
+++ Plugin/build.bat
@@ -1,3 +1,13 @@
-call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+call toolchain.bat
+
 msbuild fccore.sln /t:Build /p:Configuration=Master /p:Platform=x64 /m /nologo
+IF %ERRORLEVEL% NEQ 0 (
+    pause
+    exit /B 1
+)
+
 msbuild fccore.sln /t:Build /p:Configuration=Master /p:Platform=Win32 /m /nologo
+IF %ERRORLEVEL% NEQ 0 (
+    pause
+    exit /B 1
+)
